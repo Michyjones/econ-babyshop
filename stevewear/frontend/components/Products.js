e67a46bdfb58a1d8caf412a28a4e5cb17e037534
Michyjones <mykembuguah11@gmail.com>
2023-06-27 16:40:43 +0300

    Added product functionalities to add, fetch data from the backend

---
+++ stevewear/frontend/components/Products.js
@@ -0,0 +1,43 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Product from './Product';
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+      price
+    }
+  }
+`;
+
+export default function Products() {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  console.log(data, error, loading);
+  if (loading) return <p>LOADING....</p>;
+  if (error) return <p>{error.message}.</p>;
+  return (
+    <div>
+      <ProductStyles>
+        {data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductStyles>
+    </div>
+  );
+}
