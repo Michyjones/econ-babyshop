d88caa0874951bf7d28a3e02169cd63ec22382bd
Michyjones <mykembuguah11@gmail.com>
2023-06-28 13:43:43 +0300

    Added a fix on the reload when you add new item

--- stevewear/frontend/.next/static/chunks/webpack.js
+++ stevewear/frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "094d2d6f2e3e8477ed12"; }
+/******/ 		__webpack_require__.h = function() { return "a7d85fe086129cd6d8f9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
