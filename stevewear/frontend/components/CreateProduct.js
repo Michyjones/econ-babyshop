0bc6f3fa3ac0e01af4f63ca1b423f6f2f80de386
Michyjones <mykembuguah11@gmail.com>
2023-06-28 09:19:47 +0300

    Added create product functuion on the frontend

---
+++ stevewear/frontend/components/CreateProduct.js
@@ -0,0 +1,66 @@
+import useForm from '../lib/useForm';
+import Form from './styles/Form';
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
+    name: 'Rompers',
+    price: '2000',
+    description: 'Product description',
+  });
+  return (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(inputs);
+      }}
+    >
+      <fieldset>
+        <label htmlFor="image">
+          Image Upload:
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name:
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={inputs.name}
+            placeholder="Name"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Price:
+          <input
+            type="number"
+            id="price"
+            name="price"
+            value={inputs.price}
+            placeholder="Price"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description:
+          <textarea
+            type="text"
+            id="description"
+            name="description"
+            value={inputs.description}
+            placeholder="Description"
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
+  );
+}
