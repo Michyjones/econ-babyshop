2c0192bf8c8e1a1a6034b6decc0c2670b4d68164
Michyjones <mykembuguah11@gmail.com>
2023-07-11 18:18:02 +0300

    Added a request and reset functions on the backend

--- stevewear/backend/keystone.ts
+++ stevewear/backend/keystone.ts
@@ -5,6 +5,7 @@ import {withItemData, statelessSessions} from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { sendPasswordResetEmail } from './lib/mail';
 
  const databaseURL = process.env.DATABASE_URL || 'http://localhost/database';
 
@@ -19,6 +20,11 @@ import { ProductImage } from './schemas/ProductImage';
      secretField: 'password',
      initFirstItem: {
          fields:['name', 'email', 'password']
+     },
+     passwordResetLink: {
+        async sendToken(args){
+            await sendPasswordResetEmail(args.token, args.identity);
+        }
      }
  })
 
