6344ee6b7370923f200af94020969254c7a64151
Michyjones <mykembuguah11@gmail.com>
2023-07-20 23:39:09 +0300

    Added Stripe payment function on the backend

--- stevewear/backend/lib/formatMoney.ts
+++ stevewear/backend/lib/formatMoney.ts
@@ -1,9 +1,22 @@
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-});
-
-export default function formatMoney(cents: number) {
-  const dollars = cents / 100;
-  return formatter.format(dollars);
+export default function formatMoney(amount = 0) {
+  const options = {
+    style: 'currency',
+    currency: 'Kes',
+    minimumFractionDigits: 2,
+  };
+  if (amount % 100 === 0) {
+    options.minimumFractionDigits = 0;
+  }
+  const formatter = Intl.NumberFormat('en-US', options);
+  return formatter.format(amount * 10000);
 }
+
+// const formatter = new Intl.NumberFormat('en-KE', {
+//   style: 'currency',
+//   currency: 'kes',
+// });
+
+// export default function formatMoney(cents: number) {
+//   const ksh = cents * 10000;
+//   return formatter.format(ksh);
+// }
